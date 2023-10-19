530c7a30ffea9dcd964e486b23169a8c1ba0bdbb
James Pericles <jamesspericles@gmail.com>
2023-10-18 20:54:36 -0500

    debugging remote tests

--- src/util/sanitizeTournamentName/index.ts
+++ src/util/sanitizeTournamentName/index.ts
@@ -1,7 +1,11 @@
 const sanitizeTournamentName = (str: string): string => {
-    const words = str.split('_');
-    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
-    return capitalizedWords.join(' ');
+    if (str === undefined) {
+        console.log(str)
+        return ''
+    }
+    const words = str.split('_')
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    return capitalizedWords.join(' ')
 }
 
 export default sanitizeTournamentName
