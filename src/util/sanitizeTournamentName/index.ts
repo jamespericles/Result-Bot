a6ce36dff57c95303d2da9ef96c85c8ba50ebc4c
James Pericles <jamesspericles@gmail.com>
2023-10-18 21:16:03 -0500

    update yml

--- src/util/sanitizeTournamentName/index.ts
+++ src/util/sanitizeTournamentName/index.ts
@@ -1,8 +1,4 @@
 const sanitizeTournamentName = (str: string): string => {
-    if (str === undefined) {
-        console.log(str)
-        return ''
-    }
     const words = str.split('_')
     const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
     return capitalizedWords.join(' ')
